e1a98cbfd4dc8076875b7f098fc14d139c78e0ae
Tushar Prajapati <tusharprt5@gmail.com>
2025-06-10 06:27:27 +0530

    first commit

---
+++ react01/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Header({title}){
+  return (
+    <div>
+      {title}
+    </div>
+  )
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <Header title="Tushar"/>
+       <Header title="Name2"/>
+
+    </>
+  )
+}
+
+export default App
