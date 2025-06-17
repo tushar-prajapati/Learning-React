a3bef8cdb035e98748903ebb692436518d097705
Tushar Prajapati <tusharprt5@gmail.com>
2025-06-17 13:39:14 +0530

    add task 3

---
+++ react03/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import WelcomeMessage from './components/WelcomeMessage'
+import JSXRules from './components/JSXRules'
+
+const App = () => {
+  return (
+    <>
+    <WelcomeMessage />
+    <JSXRules />
+    </>
+  )
+}
+
+export default App
---
+++ react03/src/components/JSXRules.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const JSXRules = () => {
+  return (
+    <div>
+        <h1>JSX Rules</h1>
+        <p>
+            <ul>
+                <li>JSX must return a single parent element.</li>
+                <li>JSX element must be properly closed.</li>
+                <li>JSX attributes are written using camelCase(e.g. className instead of class).</li>
+            </ul>
+        </p>
+    </div>
+  )
+}
+
+export default JSXRules
