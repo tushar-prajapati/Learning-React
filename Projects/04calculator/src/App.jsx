d09dbfbd35393cae7f936027b53cd5407f33e131
Tushar Prajapati <tusharprt5@gmail.com>
2025-06-20 04:44:39 +0530

    add 04calculator

---
+++ Projects/04calculator/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Calculator from './Calculator'
+
+const App = () => {
+
+  return (
+    <div>
+      <Calculator />
+    </div>
+  )
+}
+
+export default App
