4c69e1037a62803096039a01dc4323c74f199fa2
Tushar Prajapati <tusharprt5@gmail.com>
2025-06-20 04:23:45 +0530

    add 02todo 03meals

---
+++ Projects/02todo/src/Todo.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState } from 'react';
+import "./style.css"
+
+const Todo = () => {
+    const [todos, setTodos] = useState([]);
+    const [input, setInput] = useState("");
+
+    const handleSubmit = () => {
+        setTodos((todos) =>
+          todos.concat({
+            text: input,
+            id: Math.floor(Math.random() * 10000),
+          })
+        );
+        setInput("");
+      };
+
+    const removeTodo = (id)=> setTodos(todos=> todos.filter(t => t.id !== id))
+  return (
+    <div className='container'>
+        <input type="text" placeholder='New Todo' value={input} onChange={e=>setInput(e.target.value)} />
+        <button onClick={handleSubmit}>Submit</button>
+        <ul className='todos-list'>
+            {todos.map(({text,id})=>(
+                <li className='todo' key={id}>
+                        <span>{text}</span>
+                        <button className='close' onClick={()=>removeTodo(id)}>x</button>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default Todo
---
+++ Projects/03meals/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Meals from './Meals'
+
+const App = () => {
+  return (
+    <div>
+      <Meals />
+    </div>
+  )
+}
+
+export default App
