19c47ded1b5382d4497aea1ffe4a180d6e0d21f0
Tushar Prajapati <tusharprt5@gmail.com>
2025-06-22 08:39:16 +0530

    fixes

--- React with TS/01/src/App.tsx
+++ React with TS/01/src/App.tsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react'
-import Button from './components/Button'
+import { useState } from 'react';
+import Button from './components/Button.tsx'
 
 const App = () => {
-  
-
+  const [disabled, setDisabled] = useState(false)
   const onClickHandler=()=>{
     alert('Hello');
   }
   return (
     <div>
-      <Button label='Click Me1' onClick={onClickHandler} disabled={false}/>
-      <Button label='Click Me2'  disabled={true}/>
+      <Button label='Click Me1' onClick={onClickHandler} disabled={disabled}/>
+      <Button label='Click Me2' onClick={onClickHandler} disabled={!disabled}/>
     </div>
   )
 }
